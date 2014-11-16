09bd8163e7c846df74b86a80ccf4a15110391083
Liampobob <liam@thamlock.com>
2014-11-15 16:54:44 -0800

    Update DriveTrain.cpp

--- DriveTrain.cpp
+++ DriveTrain.cpp
@@ -8,10 +8,53 @@ DriveTrain::DriveTrain() :
   
 {
   
-  rotateSpeed = 423.0;
-  moveSpeed = 234.0;
+  rotateSpeed = leftRightController.get();
+  moveSpeed = forwardBackwardController.get();
   
+  leftFrontVic.Set(moveSpeed);
+  leftBackVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
   
+  void DriveTrain::update(){
+    
+  
+    
+  }
+  
+  void DriveTrain::rotateLeft(){
+    
+  leftFrontVic.Set(moveSpeed);
+  leftBackVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
+    
+  }
+  
+  void DriveTrain::rotateRight(){
+    
+  leftFrontVic.Set(moveSpeed);
+  leftBackVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
+    
+  }
+  
+  void DriveTrain::stop(){
+   
+  leftFrontVic.Set(0.0);
+  leftBackVic.Set(0.0);
+  rightFrontVic.Set(0.0);
+  rightFrontVic.Set(0.0);
+    
+  }
 
-
+  void DriveTrain::move(){
+    
+  leftFrontVic.Set(moveSpeed);
+  leftBackVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
+  rightFrontVic.Set(moveSpeed);
+    
+  }
 }
